a0a3a23ec882b4ec2046bfe18b63fa6cd01da8e3
mnoah <prof.moriarty85@gmail.com>
2025-10-02 01:13:55 +0200

    lazyvim change add theme configuratin in autocmds.lua

--- lazyvim/lua/config/autocmds.lua
+++ lazyvim/lua/config/autocmds.lua
@@ -17,15 +17,15 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
--- Автокоманда для файлов C и C++
-vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "c", "cpp" }, -- Применять для файлов C и C++
-  callback = function()
-    vim.opt_local.tabstop = 4 -- Устанавливаем tabstop = 4
-    vim.opt_local.shiftwidth = 4 -- Устанавливаем shiftwidth = 4
-    vim.opt_local.expandtab = false -- Преобразуем табуляции в пробелы
-  end,
-})
+-- -- Автокоманда для файлов C и C++
+-- vim.api.nvim_create_autocmd({ "FileType" }, {
+--   pattern = { "c", "cpp", "py" }, -- Применять для файлов C и C++
+--   callback = function()
+--     vim.opt_local.tabstop = 4 -- Устанавливаем tabstop = 4
+--     vim.opt_local.shiftwidth = 4 -- Устанавливаем shiftwidth = 4
+--     vim.opt_local.expandtab = false -- Преобразуем табуляции в пробелы
+--   end,
+-- })
 
 -- Автосохранение файлов
 -- vim.api.nvim_create_autocmd({"InsertLeave", "TextChanged"}, {
@@ -35,5 +35,32 @@ vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged", "FocusLost" }, {
     if vim.bo.modified then
       vim.cmd("silent write")
     end
+  end,
+})
+
+-- Сделать цвета номеров строк в цвет комментариев
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "*",
+  callback = function()
+    -- Получаем цвет комментариев
+    local comment_hl = vim.api.nvim_get_hl(0, { name = "Comment" })
+    -- Применяем тот же цвет к номерам строк
+    vim.api.nvim_set_hl(0, "LineNr", {
+      fg = comment_hl.fg,
+      bg = comment_hl.bg or "NONE",
+    })
+	-- Имена файлов в Snack exploerr
+	vim.api.nvim_set_hl(0, "SnacksPickerGitStatusUntracked", {
+      fg = comment_hl.fg,
+      bg = comment_hl.bg or "NONE",
+    })
+
+	--SnacksPickerDir
+    -- Опционально: сделать номер текущей строки немного ярче
+    -- vim.api.nvim_set_hl(0, "CursorLineNr", {
+    -- fg = comment_hl.foreground,
+    -- bg = comment_hl.background or "NONE",
+    -- bold = true,
+    -- })
   end,
 })
