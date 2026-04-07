0000f69ab771a8c6ac896b4f0f035110665a5055
mnoah <prof.moriarty85@gmail.com>
2026-04-07 10:40:47 +0200

    adding waybar language state, yazi config

--- wezterm.lua
+++ wezterm.lua
@@ -15,7 +15,24 @@ config.font_size = 14
 config.font = wezterm.font 'Fira Code'
 -- config.font = wezterm.font("Fira Code", { italic = true })
 
+config.scrollback_lines = 1000 -- default 10000+
+config.window_background_opacity = 1.0 -- Отключи прозрачность и эффекты
+config.use_fancy_tab_bar = false -- Отключи fancy tab bar
+config.max_fps = 60 -- частоту рендера
+-- config.front_end = "Software" -- Отключи GPU (default "OpenGL")
+config.hide_tab_bar_if_only_one_tab = true
+-- config.enable_tab_bar = false
+config.show_tab_index_in_tab_bar = false
+
 -- config.color_scheme = "AdventureTime"
+
+config.color_scheme = 'ayu'
+config.color_scheme = 'Ayu Dark (Gogh)'
+
+
+
+
+
 
 -- Finally, return the configuration to wezterm:
 return config
